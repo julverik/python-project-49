5f4f204cb015aa3f24f114ac6e0760a099bce392
zxcwave <90509123+julverik@users.noreply.github.com>
2026-01-20 15:28:33 +0300

    new brain_games, delete quality gate

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,11 @@
+import prompt
 from brain_games.cli import welcome_user
 
 
 def main():
     welcome_user()
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
 
 
 if __name__ == "__main__":
